82f29e74c839e0f51ee4f9c050ddc31d4961a2eb
it212158 <99322744+it212158@users.noreply.github.com>
2022-02-09 22:21:03 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -11,20 +11,20 @@ sum = 1+2+3+....+ 99+100 = 5050
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
-#include <stdio.h>
+#include <stdio.h>//standared input output header
 int main() {
-  	int i,n,sum=0;
-	printf("enter a number :");
-	scanf("%d",&n);
+  	int i,n,sum=0;//input values
+	printf("enter a number :");//diplay enter a number
+	scanf("%d",&n);//input number to keyboard
 	
-	for(i=1; i<=n; i++)
+	for(i=1; i<=n; i++)// for loop
 	{
-		sum= sum+i;
+		sum= sum+i;//calculate sum
 		
 	}
-	printf("sum of the first %d numbers = %d",n,sum);
+	printf("sum of the first %d numbers = %d",n,sum);//diaplay sum
 	
   
   return 0;
-}
+}//end
 
