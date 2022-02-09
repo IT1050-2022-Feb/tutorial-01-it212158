f2a1353b046e6695c2cbc33dead748d7b5693c95
it212158 <99322744+it212158@users.noreply.github.com>
2022-02-09 22:26:59 +0530

    Update Tute04.c

--- Tute04.c
+++ Tute04.c
@@ -4,20 +4,20 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 
 Do not change the code given in the main() function when you are implementing your solution.*/
 
-#include <stdio.h>
+#include <stdio.h>//standared input output header
 
 int main() {
-   int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
+   int no1, no2;//input numbers
+   printf("Enter a value for no 1 : ");//display enter a value
+   scanf("%d", &no1);//input no1 to keyboard
+   printf("Enter a value for no 2 : ");//display enter a value
+   scanf("%d", &no2);// input no2 to keyboard
+   printf("%d ", minimum(no1, no2));//display enter minimum value
+   printf("%d ", maximum(no1, no2));//display enter maximum value
    printf("%d ", multiply(no1, no2));
    int minimum(n1,n2)
 {
-	if(n1 < n2)
+	if(n1 < n2)//select minimum number
 	{
 		return n1;
 	}
@@ -30,7 +30,7 @@ int main() {
 int maximum(n1,n2)
 {
 
-if(n1 > n2)
+if(n1 > n2)//select maximum number
 	{
 		return n1;
 	}
@@ -50,4 +50,4 @@ int multiply(n1,n2)
   
 
    return 0;
-}
+}//end
